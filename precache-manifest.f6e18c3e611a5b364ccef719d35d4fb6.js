fc4b2b5e062a32622611c11150d07ad1f8efb5bb
ralexwong <46982508+ralexwong@users.noreply.github.com>
2019-08-12 18:43:41 -0700

    Updates

---
+++ precache-manifest.f6e18c3e611a5b364ccef719d35d4fb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d879d75ff129d7727a9a6d4df27a44",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "1a6b6b9b226c2e4747fb",
+    "url": "/clicky-game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4dbc779420a3e2162ebe",
+    "url": "/clicky-game/static/js/2.9590dc6d.chunk.js"
+  },
+  {
+    "revision": "1a6b6b9b226c2e4747fb",
+    "url": "/clicky-game/static/js/main.738e7bba.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
