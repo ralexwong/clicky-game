2037a880abc303c617d79b617f4617c917392534
ralexwong <46982508+ralexwong@users.noreply.github.com>
2019-11-19 16:06:35 -0800

    updated layout css and format. Topscore state is still buggy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Main from './main.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const App = () => <Main />;
--- src/navbar.js
+++ src/navbar.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 function Navbar (props) {
     return (
-        <nav className="navbar">
-            <ul>
-                <li>Clicky Game</li>
-                <li>Click an image to begin!</li>
-                <li>Score: <span id="score">{props.score}</span> | Top Score: <span id="topScore">{props.highScore}</span></li>
-            </ul>
-        </nav> 
+        <Row className="top">
+            <Col>
+                Clicky Game
+            </Col>
+            <Col>
+                Click an image to begin!
+            </Col>
+            <Col>
+                Score: <span id="score">{props.score}</span> | Top Score: <span id="topScore">{props.highScore}</span>
+            </Col>
+        </Row> 
     )
 };
 
